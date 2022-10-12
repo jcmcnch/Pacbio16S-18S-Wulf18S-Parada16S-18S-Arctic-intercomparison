ec82d3a0332f4ffb50c262c210bab03c62e2c210
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-10-12 11:20:46 -0700

    Update repo

--- 220821_Taylor_MG_SSU_rRNA_non-redundant/scripts/03-convert-to-tsv-counts.sh
+++ 220821_Taylor_MG_SSU_rRNA_non-redundant/scripts/03-convert-to-tsv-counts.sh
@@ -9,7 +9,7 @@ for inputFolder in EGC-classified-SILVA138.1 WSC-classified-SILVA138.1 ; do
 			SampleID=`basename $item .classified.SILVA138.1.tsv`
 			outfile=`basename $item .tsv`.taxtable.tsv
 			printf "SampleID\t$SampleID\n" > $inputFolder/$outfile
-			sed -re 's/\([0-9]{1}.[0-9]{2}\)//g' $item | cut -f2 | sort | uniq -c | awk '{print $2,"\t",$1}' | sed 's/^ \t/NA\t/g' >> $inputFolder/$outfile
+			sed -re 's/\([0-9]{1}.[0-9]{2}\)//g' $item | cut -f2 | sort | uniq -c | awk '{print $2,"\t",$1}' | sed 's/^ \t/d:Unclassified\t/g' >> $inputFolder/$outfile
 
         done
 done
