0f511556d7b82325b8e7c51266b401ecb8fa5a1d
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2023-03-01 11:24:23 +0800

    Update repo automatically.

--- Plotting-TaxaIntercomparisons/make-3-panel-plots.R
+++ Plotting-TaxaIntercomparisons/make-3-panel-plots.R
@@ -2,4 +2,5 @@ library(phyloseq)
 library(tidyverse)
 #import file
 MG <- import_biom("output-MG-combined-OTU-table/RAS_WGC_2016-2017.PacBio.merged.biom")
-Parada <- import_biom("foo.biom")
+Parada <- import_biom("input-tag-ASV-tables/221129-1417_FRAM-Parada_2.0-fold-18S-correction_normalized_sequence_counts.biom")
+Wulf <- import_biom("input-tag-ASV-tables/221026-1554.FRAM-Wulf.18S.all-18S-seqs.with-SILVA-tax.biom")
