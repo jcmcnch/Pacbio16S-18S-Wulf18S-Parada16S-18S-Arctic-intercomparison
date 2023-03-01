a34287642e066137fd57b4e8734bb59aff286879
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2023-03-01 10:35:17 +0800

    Add update script to transfer files at regular intervals from plotting dir

---
+++ Plotting-TaxaIntercomparisons/update-from-Dropbox.sh
@@ -0,0 +1,9 @@
+cp -r /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/env/ .
+cp -r /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/scripts/ .
+cp -r /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/input-MG-sample-tables/ .
+cp -r /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/input-tag-ASV-tables/ .
+cp -r /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/associated-data/ .
+cp -r /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/metadata/ .
+cp /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/*sh setup-scripts/
+cp /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/*R .
+cp /home/jesse/Dropbox/02-Collaborations/Matthias/221129_integrative_plotting/*Rhistory .
