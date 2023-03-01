90228352fc4d395644b458db4ec0e3889514dea2
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2023-03-01 10:39:45 +0800

    Update repo automatically.

--- Plotting-TaxaIntercomparisons/scripts/02-make-biom-tables.sh
+++ Plotting-TaxaIntercomparisons/scripts/02-make-biom-tables.sh
@@ -3,3 +3,6 @@ conda activate biom-env
 biom convert -i output-MG-combined-OTU-table/RAS_WGC_2016-2017.PacBio.merged.tsv -o output-MG-combined-OTU-table/RAS_WGC_2016-2017.PacBio.merged.biom --to-hdf5 --header-key taxonomy
 
 biom convert -i input-tag-ASV-tables/221129-1417_FRAM-Parada_2.0-fold-18S-correction_normalized_sequence_counts.tsv -o input-tag-ASV-tables/221129-1417_FRAM-Parada_2.0-fold-18S-correction_normalized_sequence_counts.biom --to-hdf5 --header-key taxonomy
+
+biom convert -i input-tag-ASV-tables/221026-1554.FRAM-Wulf.18S.all-18S-seqs.with-SILVA-tax.tsv -o  input-tag-ASV-tables/221026-1554.FRAM-Wulf.18S.all-18S-seqs.with-SILVA-tax.biom --to-hdf5 --header-key taxonomy
+
