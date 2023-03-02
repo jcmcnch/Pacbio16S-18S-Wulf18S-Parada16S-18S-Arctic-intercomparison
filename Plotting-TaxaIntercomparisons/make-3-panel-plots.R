e014a5c1d2bfefcd8b61aeb31716aeaea83a32f8
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2023-03-02 11:25:49 +0800

    Update repo automatically.

--- Plotting-TaxaIntercomparisons/make-3-panel-plots.R
+++ Plotting-TaxaIntercomparisons/make-3-panel-plots.R
@@ -1,7 +1,25 @@
 library(phyloseq)
 library(tidyverse)
-#import file
+library(phylosmith)
+#import files from biom, which works now
 MG <- import_biom("output-MG-combined-OTU-table/RAS_WGC_2016-2017.PacBio.merged.biom")
 Parada <- import_biom("input-tag-ASV-tables/221129-1417_FRAM-Parada_2.0-fold-18S-correction_normalized_sequence_counts.biom")
 Wulf <- import_biom("input-tag-ASV-tables/221026-1554.FRAM-Wulf.18S.all-18S-seqs.with-SILVA-tax.biom")
 
+#next import sample data
+MGmeta <- read.delim("metadata/221110_sample_metadata.PacBio.tsv", sep = "\t", header = TRUE)
+rownames(MGmeta) <- MGmeta$SampleID
+
+#and delete the first column because it is now redundant
+MGmeta <- MGmeta %>% 
+  select(-SampleID)
+
+MGmeta <- MGmeta[order(as.Date(MGmeta$date, format="%m/%d/%Y")),]
+
+#import into phyloseq object, then merge
+MGmeta <- sample_data(MGmeta)
+MG <- merge_phyloseq(MG, MGmeta)
+MG <- set_sample_order(MG, c('RAS_WSC_09_2016_1', 'RAS_WSC_10_2016', 'RAS_WSC_11_2016', 'RAS_WSC_12_2016_1', 'RAS_WSC_03_2017_2', 'RAS_WSC_05_2017_1', 'RAS_WSC_06_2017_1', 'RAS_WSC_07_2017_1', 'RAS_WSC_07_2017_2', 'RAS_WSC_07_2017_3'))
+
+#then do some plotting
+abundance_lines(MG, classification = 'Rank1', relative_abundance = TRUE, treatment = "Sample_type")
--- Plotting-TaxaIntercomparisons/setup-scripts/00-install-phyloseq.sh
+++ Plotting-TaxaIntercomparisons/setup-scripts/00-install-phyloseq.sh
@@ -4,3 +4,9 @@
 conda create --name phyloseq-env
 conda activate phyloseq-env
 mamba install r-base r-tidyverse
+
+#entered interactive R session
+#if (!require("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install(version = "3.15")
+#BiocManager::install("phyloseq")
