e933c5e35ab4dafa4213f66876445e6f59647378
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2023-03-06 10:37:53 +0800

    Update repo automatically.

--- Plotting-TaxaIntercomparisons/make-3-panel-plots.R
+++ Plotting-TaxaIntercomparisons/make-3-panel-plots.R
@@ -3,8 +3,12 @@ library(tidyverse)
 library(phylosmith)
 #import files from biom, which works now
 MG <- import_biom("output-MG-combined-OTU-table/RAS_WGC_2016-2017.PacBio.merged.biom")
+colnames(tax_table(MG)) <- c("Domain", "Phylum", "Class", "Order", "Family", "Genus", "Species")
 Parada <- import_biom("input-tag-ASV-tables/221129-1417_FRAM-Parada_2.0-fold-18S-correction_normalized_sequence_counts.biom")
 Wulf <- import_biom("input-tag-ASV-tables/221026-1554.FRAM-Wulf.18S.all-18S-seqs.with-SILVA-tax.biom")
+
+#TODO:
+#change taxonomy headers from "Rank1, Rank2, ... etc" to something more biologically realistic.
 
 #next import sample data
 MGmeta <- read.delim("metadata/221110_sample_metadata.PacBio.tsv", sep = "\t", header = TRUE)
@@ -22,4 +26,5 @@ MG <- merge_phyloseq(MG, MGmeta)
 MG <- set_sample_order(MG, c('RAS_WSC_09_2016_1', 'RAS_WSC_10_2016', 'RAS_WSC_11_2016', 'RAS_WSC_12_2016_1', 'RAS_WSC_03_2017_2', 'RAS_WSC_05_2017_1', 'RAS_WSC_06_2017_1', 'RAS_WSC_07_2017_1', 'RAS_WSC_07_2017_2', 'RAS_WSC_07_2017_3'))
 
 #then do some plotting
-abundance_lines(MG, classification = 'Rank1', relative_abundance = TRUE, treatment = "Sample_type")
+abundance_lines(MG, classification = 'Domain', relative_abundance = TRUE, treatment = "Sample_type", sample_labels = sample_data(MG)$date)
+phylogeny_profile(MG, classification = 'Domain', treatment = "Sample_type", subset = NULL, merge = TRUE, relative_abundance = TRUE, colors = 'default')
